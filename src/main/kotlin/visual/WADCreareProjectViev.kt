147a4133423726e1d507d37a828700863d2774cc
pwshady <pwshady@gmail.com>
2022-03-17 16:40:16 +0300

    1703-1

--- src/main/kotlin/visual/WADCreareProjectViev.kt
+++ src/main/kotlin/visual/WADCreareProjectViev.kt
@@ -1,23 +1,39 @@
 package visual
 
 import io.reactivex.Observable
+import javafx.beans.property.SimpleObjectProperty
 import javafx.scene.Parent
+import javafx.scene.control.DatePicker
 import javafx.scene.control.TextArea
 import javafx.scene.control.TextField
 import javafx.scene.paint.Color
+import javafx.util.converter.LocalDateStringConverter
 import tornadofx.*
 import validation.ValidationProject
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
+import kotlin.math.max
 
 class WADCreareProjectViev : Fragment() {
     override val root: Parent = form {
-        var name: TextField by singleAssign()
-        var domenName = ""
-        var from = ""
-        var to = ""
+        var name : TextField by singleAssign()
+        var domenName : TextField by singleAssign()
+        var from : TextField by singleAssign()
+        var dateFrom : DatePicker by singleAssign()
+        var to : TextField by singleAssign()
+        var dateTo : DatePicker by singleAssign()
         var directory : TextField by singleAssign()
         var errorList : TextArea by singleAssign()
         var errorText = MutableList(5,{Pair("",0)})
+        val dateFromValue = SimpleObjectProperty<LocalDate>()
+        val dateToValue = SimpleObjectProperty<LocalDate>()
+        val min = "19970101000000"
+        val max ="20221230000000"
+        fun statusUpdating (errorText : List<Pair<String,Int>>):Unit
+        {
+            errorList.textProperty().set(errorText.map { task -> task.first }.joinToString(separator =""))
+        }
         fieldset("Create project") {
             field("Project name") {
                 name = textfield()
@@ -26,18 +42,66 @@ class WADCreareProjectViev : Fragment() {
                     println("on")
                 }
                 name.textProperty().onChange {
-                    //errorText[0] = ValidationProject.nameValidation(name.text)
-                    //errorList.textProperty().set(errorText[0])
+                    errorText.set(0, Pair(ValidationProject.nameValidation(name.text).first,ValidationProject.nameValidation(name.text).second))
+                    statusUpdating(errorText);
                 }
             }
             field("Domen name"){
-                textfield()
+                domenName = textfield()
+                errorText.set(1, Pair(ValidationProject.domenNameValidation(domenName.text).first,ValidationProject.domenNameValidation(domenName.text).second))
+                domenName.textProperty().onChange {
+                    errorText.set(1, Pair(ValidationProject.domenNameValidation(domenName.text).first,ValidationProject.domenNameValidation(domenName.text).second))
+                    statusUpdating(errorText);
+                }
             }
             field("Date from"){
-                textfield()
+                hbox {
+                    from = textfield()
+                    dateFrom = datepicker(dateFromValue){
+                        //val pattern = "yyyyMMdd"
+                        //val dateFormatter = DateTimeFormatter.ofPattern(pattern)
+                        //converter = LocalDateStringConverter(dateFormatter,dateFormatter)
+                        value = LocalDate.now()
+                    }
+                    button("min") {
+                        action {
+                            from.text = min
+                        }
+                    }
+                    dateFrom.valueProperty().onChange {
+                        from.text = "${dateFrom.value.toString().replace("-","")}000000"
+                    }
+                    errorText.set(2, Pair(ValidationProject.dateTimeValidation(from.text,min,"","from")
+                        .first,ValidationProject.dateTimeValidation(from.text,min,"", "from").second))
+                    from.textProperty().onChange {
+                        errorText.set(2, Pair(ValidationProject.dateTimeValidation(from.text,min,"","from")
+                            .first,ValidationProject.dateTimeValidation(from.text,min,"","from").second))
+                        statusUpdating(errorText);
+                    }
+                }
             }
             field("Date to"){
-                textfield()
+                hbox{
+                    to = textfield()
+                    dateTo = datepicker(dateToValue){
+                        value = LocalDate.now()
+                    }
+                    button("max") {
+                        action {
+                            to.text = max
+                        }
+                    }
+                    dateTo.valueProperty().onChange {
+                        to.text = "${dateTo.value.toString().replace("-","")}000000"
+                    }
+                    errorText.set(3, Pair(ValidationProject.dateTimeValidation(to.text,"",max,"to")
+                        .first,ValidationProject.dateTimeValidation(to.text,"",max, "to").second))
+                    to.textProperty().onChange {
+                        errorText.set(3, Pair(ValidationProject.dateTimeValidation(to.text,"",max,"to")
+                            .first,ValidationProject.dateTimeValidation(to.text,"",max, "to").second))
+                        statusUpdating(errorText);
+                    }
+                }
             }
             field("Files directory"){
                 directory = textfield()
@@ -51,9 +115,9 @@ class WADCreareProjectViev : Fragment() {
                 }
             }
             errorList.disableProperty().set(true)
-            errorList.textProperty().set(errorText.joinToString(separator = ""))
-            println("aaaaaaaaaa"+errorText.map { task -> task.first }.joinToString(separator =""))
+            statusUpdating(errorText);
         }
+
         println(name)
         hbox {
             button("Create") {
@@ -67,5 +131,6 @@ class WADCreareProjectViev : Fragment() {
                 }
             }
         }
+
     }
 }
